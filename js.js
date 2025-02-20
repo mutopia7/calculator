e189282ad2befb4b1f30b5d88c410fa61b354064
Mohammadrasool Vahid <121235436+mutopia7@users.noreply.github.com>
2025-02-20 21:57:54 +0330

    feat: Add getNum for getting number fron user and display on show section and store that number in object

--- js.js
+++ js.js
@@ -1,37 +1,70 @@
-function add(a,b){
-    console.log(a+b);
+function add(a, b) {
+    console.log(a + b);
     return a + b;
 }
 
-function subtract(a,b){
-    console.log(a-b);
-    return a-b;
+function subtract(a, b) {
+    console.log(a - b);
+    return a - b;
 }
 
-function multiply(a,b){
-    console.log(a*b);
-    return a*b;
+function multiply(a, b) {
+    console.log(a * b);
+    return a * b;
 }
 
-function divide(a,b){
-    console.log(a/b);
-    return a/b;
+function divide(a, b) {
+    console.log(a / b);
+    return a / b;
 }
 
-function operate(firstNum,operator,secoundNum){
-    switch(operator){
+function operate(firstNum, operator, secoundNum) {
+    switch (operator) {
         case "add":
-            add(firstNum,secoundNum);
+            add(firstNum, secoundNum);
             break;
         case "subtract":
-            subtract(firstNum,secoundNum);
+            subtract(firstNum, secoundNum);
             break;
         case "multiply":
-            multiply(firstNum,secoundNum);
+            multiply(firstNum, secoundNum);
             break;
         case "divide":
-            divide(firstNum,secoundNum);
+            divide(firstNum, secoundNum);
             break;
     }
 }
+
+const inputNumbers = {
+    firstNum: 0,
+    secoundNum: 0,
+
+}
+let operator = ""
+
+
+let showSection = document.querySelector("#show");
+
+function getNum() {
+    let numbers = document.querySelectorAll(".num");
+    let finalInputNum = "";
+    numbers.forEach((element) => {
+        element.addEventListener("click", function (e) {
+                inputNum = e.target.textContent;
+                finalInputNum += inputNum;
+                showSection.textContent = finalInputNum;
+                inputNumbers.firstNum = finalInputNum;  
+                console.log(finalInputNum)
+                console.log(inputNumbers)
+            // finalInputNim is string
+        })
+    });
+}
+
+
+getNum()
+
+
+
+
 
