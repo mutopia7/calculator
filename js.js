ad7fafcfab2d940a79386562578f7784df6ebd92
Mohammadrasool Vahid <121235436+mutopia7@users.noreply.github.com>
2025-02-20 18:40:50 +0330

    feat: Adding four functions for four basic operations
    Addition function for adding two inputs
    Subtraction function for subtracting two inputs
    Multiplication function for multiplying two inputs
    Division function for dividing two inputs
    
    This update will be the starting point for building our calculator

---
+++ js.js
@@ -0,0 +1,21 @@
+function add(a,b){
+    console.log(a+b);
+    return a + b;
+}
+
+function subtract(a,b){
+    console.log(a-b);
+    return a-b;
+}
+
+function multiply(a,b){
+    console.log(a*b);
+    return a*b;
+}
+
+function divide(a,b){
+    console.log(a/b);
+    return a/b;
+}
+
+
